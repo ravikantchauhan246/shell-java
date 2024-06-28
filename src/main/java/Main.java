773bddc072deca0132aa243efb8644568ac7f3b1
ravikantchauhan246 <ravikantchauhan246@gmail.com>
2024-06-28 11:05:29 +0530

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,19 @@
-// Uncomment this block to pass the first stage
 import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) throws Exception {
-        // Uncomment this block to pass the first stage
-        System.out.print("$ ");
-        
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        do{
-            System.out.println(input+": command not found");
-
-            System.out.print("$ ");
-
-            input = scanner.nextLine();
-
-            if(input.equals("exit 0")){
-                break;
-            }
-
-            if(input.startsWith("echo")){
-                System.out.println(input.substring(5));
-            }else{
-                System.out.println(input + ": command not found");
-            }
-        
-        }while(!input.matches(""));
-
-        scanner.close();
+  public static void main(String[] args) throws Exception {
+    // System.out.println("Logs from your program will appear here!");
+    while (true) {
+      System.out.print("$ ");
+      Scanner scanner = new Scanner(System.in);
+      String input = scanner.nextLine();
+      if (input.equals("exit 0")) {
+        break;
+      }
+      if (input.startsWith("echo")) {
+        System.out.println(input.substring(5));
+      } else {
+        System.out.println(input + ": command not found");
+      }
     }
+  }
 }
-
