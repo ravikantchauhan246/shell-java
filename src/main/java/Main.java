a560d38bfb57f1bf155447ec869371f879acadd1
ravikantchauhan246 <ravikantchauhan246@gmail.com>
2024-06-28 11:02:30 +0530

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,7 +22,7 @@ public class Main {
             if(input.startsWith("echo")){
                 System.out.println(input.substring(5));
             }else{
-                System.out.println(input+": command not found");
+                System.out.println(input + ": command not found");
             }
         
         }while(!input.matches(""));
